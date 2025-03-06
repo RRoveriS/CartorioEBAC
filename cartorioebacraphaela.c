3cc3b7bbafbc79e179014a17e33667334a557346
Raphaela Roveri <raphaelaroveri95@gmail.com>
2025-03-06 14:54:03 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- cartorioebacraphaela.c
+++ cartorioebacraphaela.c
@@ -79,7 +79,7 @@ int consulta()
 		
 	while(fgets(conteudo, 200, file) != NULL) //"fgets" Buscar no arquivo
 	{
-		printf("\nEssas sõo as informações do usuário: ");
+		printf("\nEssas sâo as informações do usuário: ");
 		printf("%s", conteudo);
 		printf("\n\n");
 	}
@@ -123,7 +123,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n"); 
 		printf("Opção:"); //Fim do menu
 	
 		scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -143,6 +144,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!");
+			return 0;
 			break;
 			
 			default:
